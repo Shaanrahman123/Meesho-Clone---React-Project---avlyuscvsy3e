726282b92b374dd83188b24404ca8320d1b4d46f
shaanrahman123 <coderraufur@gmail.com>
2023-04-06 18:06:15 +0530

    updated all

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+// import { createContext, useState } from "react";
+// import Navigationbar from "../component/Navigationbar";
+import Categories from "../component/Categories";
+import Header from "../component/Header";
+import List from "../List/List";
+import SetItem from "../State/SetItem";
+import Slidder from "../component/Slidder/Slidder";
+
+const Home = () => {
+
+  return (
+    <>
+      <SetItem>
+        <Header />
+        {/* <Navigationbar /> */}
+        <Slidder />
+        <Categories />
+        <List />
+      </SetItem>
+    </>
+  );
+};
+
+export default Home;
